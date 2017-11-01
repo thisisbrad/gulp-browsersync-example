72752779017506212fd06960f1154ba6d27f240c
Brad Beltowski <bradbeltowski@gmail.com>
2017-11-01 00:39:00 -0400

    Worked on README

--- gulpfile.js
+++ gulpfile.js
@@ -1,84 +1,73 @@
-const gulp        = require('gulp');
-const concat      = require('gulp-concat');
-const notify      = require('gulp-notify');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const notify = require('gulp-notify');
 const browserSync = require('browser-sync');
-const jshint      = require('gulp-jshint');
-
+const jshint = require('gulp-jshint');
 
 const paths = {
   assets: ['./src/assets/'],
-  scripts: [
-    './src/js/main.js',
-    './src/**/*.js'
-  ],
-  html: [
-  	'./src/html/*.html',
-  	'./src/html/index.html'
-  ],
-  styles: [
-  	'./src/css/styles.css',
-  	'./src/css/*.css'
-  ]
+  scripts: ['./src/js/main.js', './src/**/*.js'],
+  html: ['./src/html/*.html', './src/html/index.html'],
+  styles: ['./src/css/styles.css', './src/css/*.css']
 };
 
-gulp.task('serve', function () {
-
-    {
-      let bob = "me"
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: './public'
     }
-
-    browserSync.init({
-        server: {
-            baseDir: "./public"
-        }
-    });
+  });
 });
 
-gulp.task('js:lint', function(){
-  return gulp.src(paths.scripts)
+gulp.task('js:lint', function() {
+  return gulp
+    .src(paths.scripts)
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(notify({message: 'jshint done'}));
+    .pipe(notify({ message: 'jshint done' }));
 });
 
-gulp.task('js:build', function(){
-  return gulp.src(paths.scripts)
+gulp.task('js:build', function() {
+  return gulp
+    .src(paths.scripts)
     .pipe(concat('main.js'))
     .pipe(gulp.dest('./public/js'))
-    .pipe(notify({message: 'JS concated'}));
+    .pipe(notify({ message: 'JS concated' }));
 });
 
-gulp.task('js:watch', ['js:build'], function (done) {
-    browserSync.reload();
-    done();
+gulp.task('js:watch', ['js:build'], function(done) {
+  browserSync.reload();
+  done();
 });
 
-gulp.task('html:build', function(){
-  return gulp.src(paths.html)
+gulp.task('html:build', function() {
+  return gulp
+    .src(paths.html)
     .pipe(gulp.dest('./public/'))
-    .pipe(notify({message: 'HTML pages built'}));
+    .pipe(notify({ message: 'HTML pages built' }));
 });
 
-gulp.task('html:watch', ['html:build'], function (done) {
-    browserSync.reload();
-    done();
+gulp.task('html:watch', ['html:build'], function(done) {
+  browserSync.reload();
+  done();
 });
 
-gulp.task('css:build', function(){
-  return gulp.src(paths.styles)
+gulp.task('css:build', function() {
+  return gulp
+    .src(paths.styles)
     .pipe(concat('styles.css'))
     .pipe(gulp.dest('./public/css'))
-    .pipe(notify({message: 'CSS done'}));
+    .pipe(notify({ message: 'CSS done' }));
 });
 
-gulp.task('css:watch', ['css:build'], function (done) {
-    browserSync.reload();
-    done();
+gulp.task('css:watch', ['css:build'], function(done) {
+  browserSync.reload();
+  done();
 });
 
 gulp.task('build', ['html:build', 'js:build', 'css:build']);
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
   gulp.watch(paths.html, ['html:watch']);
   gulp.watch(paths.scripts, ['js:lint', 'js:watch']);
   gulp.watch(paths.styles, ['css:watch']);
